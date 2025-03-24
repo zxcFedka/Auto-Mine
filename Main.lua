55ac61e61a840c1f49363690c87ae275b5a161e7
zxcFedka <barsuk20367@gmail.com>
2025-03-24 11:23:04 +0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -35,3 +35,5 @@ local Window = Rayfield:CreateWindow({
       Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
    }
 })
+
+local Tab = Window:CreateTab("Main", nil) -- Title, Image
