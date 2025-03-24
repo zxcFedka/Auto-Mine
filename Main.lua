b6f069e7d860e98f029f3df2dc56d9444a65ef55
zxcFedka <barsuk20367@gmail.com>
2025-03-24 12:22:20 +0400

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,41 +1,84 @@
-if not game.PlaceId == 8737899170 then return end
+local ___0x0 =  string.char(100, 101, 99, 111, 100, 101, 100, 32, 115, 116, 114, 105, 110, 103)
+local ___0x1 = string.char(79, 117, 116, 112, 117, 116)
+local ___0x2 = string.char(82, 97, 121, 102, 105, 101, 108, 100)
+local ___0x3 = string.char(67, 114, 101, 97, 116, 101, 87, 105, 110, 100, 111, 119)
+local ___0x4 = string.char(78, 97, 109, 101)
+local ___0x5 = string.char(65, 117, 116, 111, 32, 77, 105, 110, 101)
+local ___0x6 = string.char(73, 99, 111, 110)
+local ___0x7 = string.char(76, 111, 97, 100, 105, 110, 103, 84, 105, 116, 108, 101)
+local ___0x8 = string.char(80, 105, 100, 97, 114, 97, 115, 32, 72, 117, 98)
+local ___0x9 = string.char(76, 111, 97, 100, 105, 110, 103, 83, 117, 98, 116, 105, 116, 108, 101)
+local ___0xa = string.char(98, 121, 32, 122, 120, 99, 70, 101, 100, 107, 97)
+local ___0xb = string.char(84, 104, 101, 109, 101)
+local ___0xc = string.char(68, 101, 102, 97, 117, 108, 116)
+local ___0xd = string.char(68, 105, 115, 97, 98, 108, 101, 82, 97, 121, 102, 105, 101, 108, 100, 80, 114, 111, 109, 112, 116, 115)
+local ___0xe = string.char(68, 105, 115, 97, 98, 108, 101, 66, 117, 105, 108, 100, 87, 97, 114, 110, 105, 110, 103, 115)
+local ___0xf = string.char(67, 111, 110, 102, 105, 103, 117, 114, 97, 116, 105, 111, 110, 83, 97, 118, 105, 110, 103)
+local ___0x10 = string.char(69, 110, 97, 98, 108, 101, 100)
+local ___0x11 = string.char(70, 111, 108, 100, 101, 114, 78, 97, 109, 101)
+local ___0x12 = string.char(70, 105, 108, 101, 78, 97, 109, 101)
+local ___0x13 = string.char(66, 105, 103, 32, 72, 117, 98)
+local ___0x14 = string.char(68, 105, 115, 99, 111, 114, 100)
+local ___0x15 = string.char(73, 110, 118, 105, 116, 101)
+local ___0x16 = string.char(110, 111, 105, 110, 118, 105, 116, 101, 108, 105, 110, 107)
+local ___0x17 = string.char(82, 101, 109, 101, 109, 98, 101, 114, 74, 111, 105, 110, 115)
+local ___0x18 = string.char(75, 101, 121, 83, 121, 115, 116, 101, 109)
+local ___0x19 = string.char(75, 101, 121, 83, 101, 116, 116, 105, 110, 103, 115)
+local ___0x1a = string.char(83, 117, 98, 116, 105, 116, 108, 101)
+local ___0x1b = string.char(85, 110, 116, 105, 116, 108, 101, 100)
+local ___0x1c = string.char(78, 111, 116, 101)
+local ___0x1d = string.char(78, 111, 32, 109, 101, 116, 104, 111, 100, 32, 111, 102, 32, 111, 98, 116, 97, 105, 110, 105, 110, 103, 32, 116, 104, 101, 32, 107, 101, 121, 32, 105, 115, 32, 112, 114, 111, 118, 105, 100, 101, 100)
+local ___0x1e = string.char(83, 97, 118, 101, 75, 101, 121)
+local ___0x1f = string.char(71, 114, 97, 98, 75, 101, 121, 70, 114, 111, 109, 83, 105, 116, 101)
+local ___0x20 = string.char(75, 101, 121)
+local ___0x21 = string.char(72, 101, 108, 108, 111)
+local ___0x22 = string.char(72, 111, 109, 101)
+local ___0x23 = string.char(83, 101, 99, 116, 105, 111, 110)
+local ___0x24 = string.char(77, 105, 110, 101)
+local ___0x25 = string.char(83, 101, 99, 116, 105, 111, 110, 32, 69, 120, 97, 109, 112, 108, 101)
+local ___0x26 = 8737899170
 
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+local ___0x27 = not game.PlaceId == ___0x26
+if ___0x27 then return end
 
-local Window = Rayfield:CreateWindow({
-   Name = "Auto Mine",
-   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "Pidaras Hub",
-   LoadingSubtitle = "by zxcFedka",
-   Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+local ___0x31 = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
-   DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
-
-   ConfigurationSaving = {
-      Enabled = false,
-      FolderName = nil, -- Create a custom folder for your hub/game
-      FileName = "Big Hub"
-   },
-
-   Discord = {
-      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
-
-   KeySystem = false, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "Untitled",
-      Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
+local ___0x32 = ___0x31[___0x2](___0x31) -- Rayfield:CreateWindow
+local ___0x33 = ___0x32({
+    [___0x4] = ___0x5, -- Name = "Auto Mine"
+    [___0x6] = 0,      -- Icon = 0
+    [___0x7] = ___0x8, -- LoadingTitle = "Pidaras Hub"
+    [___0x9] = ___0xa, -- LoadingSubtitle = "by zxcFedka"
+    [___0xb] = ___0xc, -- Theme = "Default"
+    [___0xd] = false, -- DisableRayfieldPrompts = false
+    [___0xe] = false, -- DisableBuildWarnings = false
+    [___0xf] = {       -- ConfigurationSaving = {
+        [___0x10] = false, -- Enabled = false
+        [___0x11] = nil,  -- FolderName = nil
+        [___0x12] = ___0x13 -- FileName = "Big Hub"
+    },
+    [___0x14] = {      -- Discord = {
+        [___0x10] = false, -- Enabled = false
+        [___0x15] = ___0x16, -- Invite = "noinvitelink"
+        [___0x17] = true  -- RememberJoins = true
+    },
+    [___0x18] = false, -- KeySystem = false
+    [___0x19] = {      -- KeySettings = {
+        [___0x4] = ___0x1b, -- Title = "Untitled"
+        [___0x1a] = ___0x1, -- Subtitle = "Система Ключей"
+        [___0x1c] = ___0x1d, -- Note = "No method of obtaining the key is provided"
+        [___0x12] = ___0x20, -- FileName = "Key"
+        [___0x1e] = true,  -- SaveKey = true
+        [___0x1f] = false, -- GrabKeyFromSite = false
+        [___0x20] = {[1] = ___0x21} -- Key = {"Hello"}
+    }
 })
 
-local Tab = Window:CreateTab("Home", nil) -- Title, Image
-local Section = Tab:CreateSection("Mine")
-Section:Set("Section Example")
+local ___0x34 = ___0x33.CreateTab
+local ___0x35 = ___0x34(___0x22, nil) -- Tab = Window:CreateTab("Home", nil)
+local ___0x36 = ___0x35.CreateSection
+local ___0x37 = ___0x36(___0x24) -- Section = Tab:CreateSection("Mine")
+local ___0x38 = ___0x37.Set
+___0x38(___0x25) -- Section:Set("Section Example")
+
+print(___0x0 .. ": " .. ___0x1) -- Debug output -  "decoded string: Output"
